68356d4eec41c369d01712ef676553fe3cbedd64
ABHINAV <abhinav30may@gmail.com>
2022-11-13 15:04:52 +0530

    added capitalization on category wise healines

--- src/components/News.js
+++ src/components/News.js
@@ -15,11 +15,14 @@ export class News extends Component {
     pageSize: PropTypes.number,
     category: PropTypes.string,
   }
+   capitalizeFirstLetter = (string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   
 
     
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         
         this.state = {
             articles: [],
@@ -29,114 +32,49 @@ export class News extends Component {
             
 
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
     }
-
-    async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7b4054165e50428598ddca8bc847ad23&page=1&pageSize=${this.props.pageSize}`;
+    async updateNews(){
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7b4054165e50428598ddca8bc847ad23&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({loading:true});
         let data =  await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
         this.setState({articles: parsedData.articles, totalResults:parsedData.totalResults,
-        loading: false})
+        loading: false}) 
+
     }
+    async componentDidMount(){
+        this.updateNews();
+    } 
 
          handlePrevClick = async()=>{
-        console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7b4054165e50428598ddca8bc847ad23&page=${this.state.page - 1}
-        {this.props.pageSize}`;
-        let data =  await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
         
-            this.setState({
-              page: this.state.page - 1,
-              articles: parsedData.articles,
-              loading: false
-            })
+        this.setState({page: this.state.page - 1})
+        this.updateNews();
         }
 
 
          handleNextClick = async()=>{
           
             console.log("Next");
-          //   console.log(Math.ceil(this.state.page))
-          //   if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
-          //     this.setState(
-          //       {
-          //         page: this.state.page+1
-          //       }
-          //     )
+         
 
-          //     console.log(this.state.page)
-          //   }
-          //   else{
-
-            
-          //       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&category=${this.props.category}&apiKey=7b4054165e50428598ddca8bc847ad23&page=${this.state.page + 1}&pageSize=20`;
-                
-          //   let data =  await fetch(url);
-          //   let parsedData = await data.json()
-          //   console.log(parsedData);
-            
-          //       this.setState({
-          //         page: this.state.page + 1,
-          //         articles: parsedData.articles
-          //       })
-          // }
-
-          //get current page by total results
-         let  pageResults = this.state.totalResults / 30.00
-         let  currentPage = this.state.page
-
-          if (!currentPage >= pageResults){
-            console.log("No More Next")
-           
-          }
-          else{
-            console.log("There are more pages")
-            console.log(this.state.page)
-           
-          
-            this.setState({
-              page: this.state.page + 1,
-            },async () =>{
-              let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7b4054165e50428598ddca8bc847ad23&page=${this.state.page}&pageSize=30`;
-              this.setState({loading: true});
-                
-              let data =  await fetch(url);
-              let parsedData = await data.json()
-              this.setState({loading: false});
-              console.log(parsedData);
-              this.setState(
-                {
-                  articles: parsedData.articles
-                }
-              )
-              if (currentPage + 1 > pageResults){
-                console.log("No More Next")
-                
-               
-              }
-            })
-            // console.log(this.state.page)
-          }
-
-
-
-        }
+        this.setState({page: this.state.page + 1 }); 
+        this.updateNews();
+         }
     
   render(){
    
     return (
       <div className="container my-4">
-        <h1 className='text-center' style={{margin: '35px 0px'}}>ROZANA - Top Headlines</h1>
+        <h1 className='text-center' style={{margin: '35px 0px'}}>ROZANA - Top {this.capitalizeFirstLetter(this.props.category)} Headlines </h1>
         {this.state.loading && <Spinner/>}
         
         <div className="row">
         {!this.state.loading && this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
-                <Newsitem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageurl ={element.urlToImage} newsurl={element.url}/>
+                <Newsitem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageurl ={element.urlToImage} newsurl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
        </div>
        })}
         </div>
@@ -149,7 +87,9 @@ export class News extends Component {
       </div>
     )
   }
+
 }
+
 
 
 export default News
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react' // (className based components)rce then will get props by this.props
+import React, { Component } from "react"; // (className based components)rce then will get props by this.props
 
 export class Newsitem extends Component {
   render() {
-   let  {title,description, imageurl,newsurl} = this.props;
+    let { title, description, imageurl, newsurl, author, date , source } = this.props;
     return (
       <div className="my-3">
-       <div className="card" >
-            <img src={!imageurl?"https://images.moneycontrol.com/static-mcnews/2022/09/Edtech-failure-Illustration-by-Suneesh-K-770x433.jpg":imageurl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title"> {title}...</h5>
-                <p className="card-text">{description}...</p>
-                <a rel="noreferrer" href={newsurl} className="btn btn-dark">Read More</a>
-            </div>
-            </div>
+        <div className="card">
+          <img
+            src={
+              !imageurl
+                ? "https://images.moneycontrol.com/static-mcnews/2022/09/Edtech-failure-Illustration-by-Suneesh-K-770x433.jpg"
+                : imageurl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {" "}
+              {title}
+              <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90', zIndex:'1'}}>
+                {source}
+                99+
+              
+              </span>
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text text-danger">
+                By {!author ? "unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a rel="noreferrer" href={newsurl} className="btn btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitem
-
-
-
-
+export default Newsitem;
